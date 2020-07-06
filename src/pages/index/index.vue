8505bdc980e9cd04d9895950e9e6d99de0bf449e
zhangmx <819251701@qq.com>
2020-07-06 19:06:21 +0800

    封面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,8 +8,10 @@
                     <div class="page book-content">end</div>
                     <div class="page book-content book-content1">第三页</div>
                     <div class="page book-content book-content2">第二页</div>
-                    <div class="page book-content book-content3">第一页</div>
-                    <div :class="cover ? 'page book-cover coverTrue' : 'page book-cover coverFalse'" @click="coverClick">封面</div>
+                    <div class="page book-content" :class="page1 ? 'page1True' : 'page1False'" @click="page1Click">第一页</div>
+                    <div :class="cover ? 'cover-bg coverTrue' : 'cover-bg coverFalse'" @click="coverClick">
+                        <div class="book-cover"></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,7 +22,11 @@
 export default {
     data () {
         return {
-            cover: false
+            cover: false,
+            page1: false,
+            page2: false,
+            page3: false
+
         }
     },
     created () {
@@ -32,6 +38,9 @@ export default {
     methods: {
         coverClick() {
             this.cover = !this.cover;
+        },
+        page1Click() {
+            this.page1 = !this.page1;
         }
     }
 }
@@ -53,6 +62,9 @@ export default {
             height: 100%;
             background: url('../../../static/img/black-1.jpg');
             background-size: 100% 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
 
         .perspective {
@@ -81,12 +93,29 @@ export default {
             justify-content: center;
             align-items: center;
             transform-origin: left;
-            border: 1px solid #1976D2;
+            // border: 1px solid #1976D2;
+        }
+
+        .cover-bg {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            left: 0;
+            top: 0;
+            font-size: 30px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transform-origin: left;
         }
 
         .book-cover {
-            background-color: #1976D2;
-            color: #ffffff;
+            // background-color: #1976D2;
+            // color: #ffffff;
+            width: 100%;
+            height: 100%;
+            background: url('../../../static/img/yuwen.jpg');
+            background-size: 100% 100%;
         }
 
         .coverTrue {
@@ -108,8 +137,26 @@ export default {
         }
 
         .book-content {
-            background-color: #fff;
+            background: linear-gradient(to bottom right, #fff, #ccc);
             color: #33363C;
+        }
+
+        .page1True {
+            animation: page1True 5s ease 0s 1 forwards;
+        }
+
+        .page1False {
+            animation: page1False 5s ease 0s 1 forwards;
+        }
+
+        @keyframes page1True {
+            from {transform: rotateY(-180deg)}
+            to {transform: rotateY(0)}
+        }
+
+        @keyframes page1False {
+            from {transform: rotateY(0)}
+            to {transform: rotateY(-180deg)}
         }
 
         .book-content1{
@@ -119,14 +166,5 @@ export default {
         .book-content2 {
             animation: roll 4s ease 2s 2 alternate;
         }
-
-        .book-content3 {
-            animation: roll 5s ease 1s 2 alternate;
-        }
-
-        // @keyframes roll {
-        //     from {transform: rotateY(0)}
-        //     to {transform: rotateY(-180deg)}
-        // }
     }
 </style>
